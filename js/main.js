b8baf1df964b3b28a9c7f7f9036b4368567ad8e1
marosmatej <114649931+marosmatej@users.noreply.github.com>
2022-11-13 23:45:04 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
+/*
 import OpinionsHandlerMustache from "./opinionsHandlerMustache.js";
 
 window.opnsHndlr = new OpinionsHandlerMustache("openForm","opinionsContainer","mTmplOneOpinion");
+window.opnsHndlr.init();
+*/
+import OpinionsHandler from "./opinionsHandler.js";
+
+window.opnsHndlr = new OpinionsHandler("openForm","opinionsContainer");
 window.opnsHndlr.init();
